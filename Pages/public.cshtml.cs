4a9c8112715b380261c08ead2ce6a053d54b04eb
toxumuharudev <toxumuharudev@toxumuharudev.work>
2023-02-26 16:16:34 +0900

    new file:   Pages/public.cshtml 	new file:   Pages/public.cshtml.cs 	deleted:    Pages/public/Index.cshtml

---
+++ Pages/public.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _024_ClientCertificationOnAppService.Pages;
+
+public class PublicModel : PageModel
+{
+    private readonly ILogger<PublicModel> _logger;
+
+    public PublicModel(ILogger<PublicModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
